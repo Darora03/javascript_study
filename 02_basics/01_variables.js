8a856dbd32f6463329e84ec652cc45164c61b3bf
Devkaran Aroa <104886931+Darora03@users.noreply.github.com>
2024-06-08 11:19:21 +0000

    variable config done

---
+++ 02_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let accountEmail="aroradk@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=2;
+accountEmail="dk@dk.com"
+accountPassword="21512"
+accountCity="Bangalore"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
